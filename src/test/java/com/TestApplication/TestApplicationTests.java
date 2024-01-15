cb4c41c4e4b02118cd82f7f7a451efa4155c4532
igboBoydev <abelkelly6022@gmail.com>
2024-01-15 17:18:09 +0100

    added comments to code

--- src/test/java/com/TestApplication/TestApplicationTests.java
+++ src/test/java/com/TestApplication/TestApplicationTests.java
@@ -28,12 +28,17 @@ class TestApplicationTests {
     @Test
     void itShouldCheckIFBookExistsUuid(){
         String uuid = String.valueOf(UUID.randomUUID());
+        //TODO: create a book model
         Books books = new Books("available", "API test 101", uuid, "John Doe");
-        this.booksRepository.save(books);
 
+        //TODO: save model to books database
+        booksRepository.save(books);
+
+        //TODO: get book by uuid
         Optional<Books> books1 = booksRepository.getBookByUuid(uuid);
         boolean isExist = books1.isPresent();
 
+        //TODO: assert that book with uuid provided exists on the database
         AssertionsForClassTypes.assertThat(isExist).isTrue();
     }
 
